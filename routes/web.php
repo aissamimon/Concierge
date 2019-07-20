a468fc00202ad8b2c639852f7cba77ea858f445e
Yusef Aissa <yusefaissa@gmail.com>
2019-07-20 16:39:38 +0200

    Implement function to create/delete new Incidents associated with the type of incident - Implemented function Update type of incident

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/', function () {
     return view('auth.login');
 });
 
-// Auth::routes(['register' => false]);
+Auth::routes(['register' => true]);
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login');
 Route::post('logout', 'Auth\LoginController@logout')->name('logout');
